96b8ecac6ed2e1888321891f36aad5c6a6741a34
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2023-11-18 00:50:24 +0000

    Update for Night Watch

--- README.md
+++ README.md
@@ -5,18 +5,18 @@ Time to make my own toolchain.
 
 ## Message for DB challenge
 
-Desert Bus 'themed' Only Connect fopr Alpha Flight
+Night Watch 'themed' Only Connect for Night Watch
 
-Hi, I'm Kitteh, and here is my Only Connect which is mojority DB themed.
-Being a bit of a stickler for the rules, and a developer, I've built a web app to play on during my downtime this week...and started building Jackbox from first principles.
+Hi, I'm [name], writing on behalf of Kitteh (who's asleep).
+This is an automated Only Connect themed (mostly) around Desert Bus and this year's Night Watch (y'all have memory of that right?).
+It consists of 2 rounds of 6 questions each, in a semi-automated website that allows a GM (engineeeing, stage friend of co-pilot) to decide if guesses are close enough.
 
-Hopefully next year I shall return with a more polished system. Thank you so much for bus, and I hope you enjoy the game.  
-- Kitteh (in chat as thirsty_kitteh or cuddly_kitteh or two_distinct_kitteh or [trail off])
+Alpha Flight scored 15 on their board -- there are 3 questions that overlap.
 
 ---
 
-Stage Friend / Tech notes: the game is at https://db.tea-cats.co.uk/, labelled 'OnlyConnect for AlphaFlight'.
+Stage Friend / Tech notes: the game is at https://db.tea-cats.co.uk/, labelled 'OnlyConnect for Night Watch'.
 The audit button will show you all the questions and answers in the game, and the play with one team will create a new instance of the game, and take you to the 'secret' DM url.
-You can either crop the marked area at the bottom, or open the public page on another tab / machine to show the enterainers and stream!
+You can either crop the marked area at the bottom, or open the public page on another tab / machine to show the entertainers and stream! (which you can also crop to the red box if you want more screen space)
 
 I request and highly encourage feedback on the concept to bus@tea-cats.co.uk.
--- src/db_games/only_connect/__init__.py
+++ src/db_games/only_connect/__init__.py
@@ -76,17 +76,27 @@ class OnlyConnectGame(Game, ABC):
 
     @classmethod
     async def audit(cls) -> web.Response:
-        question_text = [
+        connection_text = [
             (
                 f"<tr><td>{question.credit}</td>"
-                f"<td>{'sequence' if question.is_sequence else 'connections'}</td>"
                 f"<td>{question.connection}</td><td>"
                 f"<ul>"
                 + "".join([f"<li>{clue}</li>" for clue in question.clues])
                 + "</ul></td>"
                 "</tr>"
             )
-            for question in cls.possible()
+            for question in cls.possible() if not question.is_sequence
+        ]
+        sequence_text = [
+            (
+                f"<tr><td>{question.credit}</td>"
+                f"<td>{question.connection}</td><td>"
+                f"<ul>"
+                + "".join([f"<li>{clue}</li>" for clue in question.clues])
+                + "</ul></td>"
+                "</tr>"
+            )
+            for question in cls.possible() if question.is_sequence
         ]
 
         return web.Response(
@@ -94,11 +104,15 @@ class OnlyConnectGame(Game, ABC):
             body=(
                 "<!DOCTYPE html><html><head>"
                 '<meta charset="utf-8">'
-                '<link rel="stylesheet" href="style.css">'
+                '<link rel="stylesheet" href="/style.css">'
                 "</head><body>"
                 "<h1>Only Connect</h1>"
-                "<p>6 connections and 6 sequences questions will be random selected from:</p>"
-                f'<table>{"".join(question_text)}</table>'
+                "<h2>Round 1 Connections</h2>"
+                f"<p>6 of {len([x for x in cls.possible() if not x.is_sequence])} connections from:</p>"
+                f'<table>{"".join(connection_text)}</table>'
+                "<h2>Round 2 Sequences</h2>"
+                f"<p>6 of {len([x for x in cls.possible() if x.is_sequence])} sequences from:</p>"
+                f'<table>{"".join(sequence_text)}</table>'
                 "</body></html>"
             ),
         )
@@ -213,6 +227,43 @@ class OnlyConnectGame(Game, ABC):
 
 
 GENERAL_QUESTIONS = [
+    # Question(
+    #     False,
+    #     "Adjectives that prefix Kitteh's Twitch accounts",
+    #     ("thirsty_", "Cuddly_", "disaster_", "two_distinct_")
+    # )
+]
+
+NIGHTWATCH_QUESTIONS = [
+    Question(
+        False,
+        'Reasons "JULIE!!" was yelled on the first 2023 Night Watch shift',
+        (
+            "Look, we all make certain realisations at different paces.",
+            "I can't stop thinking about her",
+            "Can somebody donate exactly $80 so that the overlay reads 'boobs'?",
+            "We slapped a pair of big stonkin' tits on it",
+        ),
+        "Kitteh, with help from Julie's discord",
+    ),
+    Question(
+        False,
+        "Major phrases/memes of past Desert Bus",
+        ("VVVVVV", "Patience", "Thwomp", "Belopa"),
+        "Kitteh",
+    ),
+    Question(
+        False,
+        "'This' categories from Night Watch 'This or Thats'",
+        ("Planets in Doctor Who", "Real Flag", "Business Acryonyms", "Gundam Characters"),
+        "Kitteh",
+    ),
+    Question(
+        False,
+        "'New' genres reported by Archibald Metropolis (Broken News Day 5)",
+        ("Hyperloop", "Inert Gas", "Sponge-like Metal", "Country"),
+        "Kitteh",
+    ),
     Question(
         False,
         "Titles of the full Random Dance Party tracks",
@@ -226,33 +277,28 @@ GENERAL_QUESTIONS = [
     ),
     Question(
         False,
-        "Epithets of World of Darkness games (Wraith, Hunter, Mage, Vampire)",
-        ("Ascension", "Oblivion", "Reckoning", "Masquerade"),
-        "Kitsune",
-    ),
-    Question(
-        False,
         "Forts on Vancouver Island",
         ("Rupert", "San Miguel", "Rodd Hill", "Victoria"),
         "Kitteh",
     ),
+
     Question(
-        False,
-        "Mozilla Products",
-        ("SeaMonkey", "Thunderbird", "Bugzilla", "FireFox"),
-        "Kitteh",
+        True,
+        "Inspiration for the names of the DBZ Ginyu Force members, from shortest to tallest",
+        ("yoghurt (~100cm)", "cheese (~170cm)", "cream (~230cm)", "butter (~250cm)"),
+        "Laogeodritt",
     ),
     Question(
-        False,
-        "Desert Bus intro parodies",
-        ("Cowboy Bebop", "Bob Ross's Joy of Painting", "The Good Place", "Hamilton"),
+        True,
+        "Characters Graham has portrayed on shift intros by year",
+        (
+            "2015: Bayley",
+            "2016: Bobby Roode",
+            "2017: Finn Balor",
+            "2018: Sterling Archer",
+        ),
         "drFox17",
     ),
-    # Question(
-    #     False,
-    #     "Adjectives that prefix Kitteh's Twitch accounts",
-    #     ("thirsty_", "Cuddly_", "disaster_", "two_distinct_")
-    # )
     Question(
         True,
         "Synonyms for the main Desert Bus shifts, in reverse order",
@@ -288,6 +334,78 @@ GENERAL_QUESTIONS = [
     ),
     Question(
         True,
+        "Highest live-auction amounts for craft-along items",
+        (
+            "The Summoner's Shrine (Schala-Kitty)",
+            "General Leia Cross Stitch (Sarah Overall)",
+            "Breath of the Wild Cross Stitch (Sarah Overall)",
+            "Link to the Past Coffee Table (Chris and Emily von Seele)"
+        ),
+        "Kitteh"
+    )
+]
+
+ALPHAFLIGHT_QUESTIONS = [
+    Question(
+        False,
+        "Titles of the full Random Dance Party tracks",
+        (
+            "A Thousand Miles",
+            "In The End",
+            "Livin' On A Prayer",
+            "We Like to Party! (The Vengabus) [2020 DJ Coriander Remix]",
+        ),
+        "drFox17 + Kitteh",
+    ),
+    Question(
+        False,
+        "Epithets of World of Darkness games (Wraith, Hunter, Mage, Vampire)",
+        ("Ascension", "Oblivion", "Reckoning", "Masquerade"),
+        "Kitsune",
+    ),
+    Question(
+        False,
+        "Forts on Vancouver Island",
+        ("Rupert", "San Miguel", "Rodd Hill", "Victoria"),
+        "Kitteh",
+    ),
+    Question(
+        False,
+        "Mozilla Products",
+        ("SeaMonkey", "Thunderbird", "Bugzilla", "FireFox"),
+        "Kitteh",
+    ),
+    Question(
+        False,
+        "Desert Bus intro parodies",
+        ("Cowboy Bebop", "Bob Ross's Joy of Painting", "The Good Place", "Hamilton"),
+        "drFox17",
+    ),
+    Question(
+        False,
+        "Readings on Alpha Flight this year",
+        (
+            "They're Made of Meat",
+            "Blobfish Falls In Love With Its Reflection",
+            "The Day the Crayons Quit",
+            "Fox in Socks",
+        ),
+        "Kitteh",
+    ),
+
+    Question(
+        True,
+        "Synonyms for the main Desert Bus shifts, in reverse order",
+        (
+            "(eg) 6th Star (of a constellation)",
+            "(eg) Astronomers",
+            "(eg) Canadian Superhero Team",
+            "(eg) Morning Patrol",
+        ),
+        "Kitteh",
+    ),
+    Question(
+        True,
         "Number of times letters appear in Canadian provinces abbreviations",
         ("1: A, E, K, L, M, O, P, Q", "2: C,S", "3: (none)", "4: B, N"),
         "Kitteh (who is truly sorry)",
@@ -315,69 +433,6 @@ GENERAL_QUESTIONS = [
         "Shape at the bottom of the DB shift banners",
         ("Horizontal Line", "Line slanted upwards to the right",
          "Line going up, then back down", "Line going down, then back up"),
-        "Kitteh",
-    ),
-
-    # Question(
-    #     True,
-    #     "Highest live-auction amounts for craft-along items",
-    #     (
-    #         "The Summoner's Shrine (Schala-Kitty)",
-    #         "General Leia Cross Stitch (Sarah Overall)",
-    #         "Breath of the Wild Cross Stitch (Sarah Overall)",
-    #         "Link to the Past Coffee Table (Chris and Emily von Seele)"
-    #     ),
-    #     "Kitteh"
-    # )
-]
-
-NIGHTWATCH_QUESTIONS = [
-    Question(
-        False,
-        'Reasons "JULIE!!" was yelled on the first 2023 Night Watch shift',
-        (
-            "Look, we all make certain realisations at different paces.",
-            "I can't stop thinking about her",
-            "Can somebody donate exactly $80 so that the overlay reads 'boobs'?",
-            "We slapped a pair of big stonkin' tits on it",
-        ),
-        "Kitteh with help from Julie's discord",
-    ),
-    Question(
-        False,
-        "Major memes of past Desert Bus",
-        ("VVVVVV", "Patience", "Thwomp", "Belopa"),
-        "Kitteh",
-    ),
-    Question(
-        True,
-        "Inspiration for the names of the DBZ Ginyu Force members, from shortest to tallest",
-        ("yoghurt (~100cm)", "cheese (~170cm)", "cream (~230cm)", "butter (~250cm)"),
-        "Laogeodritt",
-    ),
-    Question(
-        True,
-        "Characters Graham has portrayed on shift intros by year",
-        (
-            "2015: Bayley",
-            "2016: Bobby Roode",
-            "2017: Finn Balor",
-            "2018: Sterling Archer",
-        ),
-        "drFox17",
-    ),
-]
-
-ALPHAFLIGHT_QUESTIONS = [
-    Question(
-        False,
-        "Readings on Alpha Flight this year",
-        (
-            "They're Made of Meat",
-            "Blobfish Falls In Love With Its Reflection",
-            "The Day the Crayons Quit",
-            "Fox in Socks",
-        ),
         "Kitteh",
     ),
 ]
